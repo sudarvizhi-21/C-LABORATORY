5f2c571c80106a5a002b1cd3e9b3d93ff44be2f7
sudarvizhi-21 <sudarvizhi258@gmail.com>
2026-09-18 20:27:01 +0530

    Add files via upload

---
+++ EXP 10/CustomDialog.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MDIApplication
+{
+    public class CustomDialog : Form
+    {
+        private TextBox nameTextBox;
+        private TextBox ageTextBox;
+        private Button okButton;
+        private Button cancelButton;
+
+        public string StudentName
+        {
+            get { return nameTextBox.Text; }
+        }
+
+        public string StudentAge
+        {
+            get { return ageTextBox.Text; }
+        }
+
+        public CustomDialog()
+        {
+            Text = "Student Details";
+            Width = 400;
+            Height = 250;
+            StartPosition = FormStartPosition.CenterParent;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+
+            Label nameLabel = new Label();
+            nameLabel.Text = "Name:";
+            nameLabel.Location = new Point(40, 40);
+            nameLabel.AutoSize = true;
+
+            nameTextBox = new TextBox();
+            nameTextBox.Location = new Point(120, 35);
+            nameTextBox.Width = 200;
+
+            Label ageLabel = new Label();
+            ageLabel.Text = "Age:";
+            ageLabel.Location = new Point(40, 80);
+            ageLabel.AutoSize = true;
+
+            ageTextBox = new TextBox();
+            ageTextBox.Location = new Point(120, 75);
+            ageTextBox.Width = 200;
+
+            okButton = new Button();
+            okButton.Text = "OK";
+            okButton.Location = new Point(120, 130);
+            okButton.Width = 80;
+
+            cancelButton = new Button();
+            cancelButton.Text = "Cancel";
+            cancelButton.Location = new Point(220, 130);
+            cancelButton.Width = 80;
+
+            okButton.Click += OkButton_Click;
+            cancelButton.Click += CancelButton_Click;
+
+            Controls.Add(nameLabel);
+            Controls.Add(nameTextBox);
+            Controls.Add(ageLabel);
+            Controls.Add(ageTextBox);
+            Controls.Add(okButton);
+            Controls.Add(cancelButton);
+
+            AcceptButton = okButton;
+            CancelButton = cancelButton;
+        }
+
+        private void OkButton_Click(object? sender, EventArgs e)
+        {
+            if (nameTextBox.Text == "" || ageTextBox.Text == "")
+            {
+                MessageBox.Show(
+                    "Please enter Name and Age.",
+                    "Validation",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void CancelButton_Click(object? sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
