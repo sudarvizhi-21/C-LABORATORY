03baf94560bbccc54e0d6a1c7da7fdfc41f55767
sudarvizhi-21 <sudarvizhi258@gmail.com>
2026-09-18 16:08:27 +0530

    Add files via upload

---
+++ EXP 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Number
+{
+    public int x, y;
+
+    public Number(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    public static Number operator +(Number n1, Number n2)
+    {
+        return new Number(n1.x + n2.x, n1.y + n2.y);
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("X Value : " + x);
+        Console.WriteLine("Y Value : " + y);
+    }
+
+    static void Main()
+    {
+        Number n1 = new Number(10, 20);
+        Number n2 = new Number(30, 40);
+
+        Number n3 = n1 + n2;
+
+        Console.WriteLine("First Object");
+        n1.Show();
+
+        Console.WriteLine("\nSecond Object");
+        n2.Show();
+
+        Console.WriteLine("\nResult After Operator Overloading");
+        n3.Show();
+
+        Console.ReadLine();
+    }
+}
